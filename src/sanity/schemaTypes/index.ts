4b63029068c6185bc11805465a67a95762e11340
shaadq <shaadqureshi8655@gmail.com>
2025-11-26 23:22:11 +0530

    sanity default schemas removed

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,9 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import { accessory } from './accessory'
-import { authorType } from './authorType'
-import { blockContentType } from './blockContentType'
-import { categoryType } from './categoryType'
 import { color } from './color'
 import { colorOption } from './colorOption'
 import { cta } from './cta'
@@ -14,21 +11,12 @@ import { hero } from './hero'
 import { landingPage } from './landingPage'
 import { navItem } from './navItem'
 import { newsletter } from './newsletter'
-import { newType } from './newType'
-import { postType } from './postType'
 import { productInfo } from './productInfo'
 import { statItem } from './statItem'
 import { testimonial } from './testimonial'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    // Default schema types
-    blockContentType,
-    categoryType,
-    postType,
-    authorType,
-    newType,
-
     // Custom schema types
     header,
     footer,
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -3,15 +3,9 @@ import type { StructureResolver } from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('Layout')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
-      S.documentTypeListItem('new').title('New'),
       S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author', "new"].includes(item.getId()!),
-      ),
+      ...S.documentTypeListItems()
     ])
 
